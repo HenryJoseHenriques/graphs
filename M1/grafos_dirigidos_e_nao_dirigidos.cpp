b775e27c0ffaf6ba08f0454a1c0d7041c6a5540b
Henry José <henryjose@edu.univali.br>
2025-08-12 08:14:04 -0300

    primeiro algoritmo de grafo dirigido ou não dirigido

---
+++ M1/grafos_dirigidos_e_nao_dirigidos.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+using namespace std;
+
+int **criarMatriz(int tam){
+    int **matriz;
+    matriz = new int*[tam];
+    for(int i = 0; i < tam;i++){
+        matriz[i] = new int[tam];
+    }
+    for(int i = 0; i < tam; i++){
+        for(int j = 0; j < tam; j++){
+            matriz[i][j] = 0;
+        }
+    }
+    return matriz;
+}
+
+void liberarMemoria(int **matriz, int tam){
+    for(int i = 0; i < tam;i++){
+        delete [] matriz[i];
+    }
+    delete[] matriz;
+}
+
+void preecherdirigida(int **matriz, int tam){
+    bool trava = true;
+    int i,j;
+    while(trava){
+        cout << "\nIndique a conexão desejada da linha/coluna.(-1 para finalizar)\n";
+        cin >> i;
+        if(i <= -1) break;
+        i = i-1;
+        
+        cout << "\nIndique a conexão desejada da coluna.(-1 para finalizar)\n";
+        cin >> j;
+        if(j <= -1) break;
+        j = j-1;
+        
+        matriz[i][j] = 1;
+        
+    }
+}
+
+void preecherNdirigida(int **matriz, int tam){
+    bool trava = true;
+    int i,j;
+    while(trava){
+        cout << "\nIndique a conexão desejada da linha/coluna.(-1 para finalizar)\n";
+        cin >> i;
+        if(i <= -1) break;
+        i = i-1;
+        
+        cout << "\nIndique a conexão desejada da coluna.(-1 para finalizar)\n";
+        cin >> j;
+        if(j <= -1) break;
+        j = j-1;
+        
+        matriz[i][j] = 1;
+        matriz[j][i] = 1;
+    }
+}
+
+void exibirMatriz(int **matriz, int tam){
+    for(int i = 0; i < tam; i++){
+        for(int j = 0; j < tam; j++){
+            cout << matriz[i][j] << " ";
+        }
+        cout << "\n";
+    }
+}
+
+void preencherMatriz(int **matriz, int tam){
+    char respodrg;
+    cout << "\nÉ dirigido?(S ou N)\n";
+    cin >> respodrg;
+    if(respodrg != 'S' && respodrg != 'N'){
+        cout << "\nResposta não aceita.\n";
+    }else if(respodrg == 'S'){
+        preecherdirigida(matriz, tam);
+    }else if(respodrg == 'N'){
+        preecherNdirigida(matriz, tam);
+    }
+}
+
+int main()
+{
+    int **matriz;
+    int tam;
+    cout << "\nEscreva o tamanho da matriz:\n";
+    cin >> tam;
+    matriz = criarMatriz(tam);
+    preencherMatriz(matriz,tam);
+    exibirMatriz(matriz,tam);
+    liberarMemoria(matriz,tam);
+    return 0;
+}
